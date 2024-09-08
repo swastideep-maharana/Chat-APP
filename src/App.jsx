d72641b01a2b2cfb6562f7319e76dd7955fa86a6
swastideep maharana <swastideep67maharana@gmail.com>
2024-09-08 11:42:29 +0530

    #added the ui of signup and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Chat from "./pages/Chat/Chat";
+import ProfileUpdate from "./pages/Profileupdate/ProfileUpdate";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<ProfileUpdate />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
