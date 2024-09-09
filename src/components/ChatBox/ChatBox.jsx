48b7dffb87e481c543b28d4cd7662fdb685a07ce
swastideep maharana <swastideep67maharana@gmail.com>
2024-09-09 13:56:39 +0530

    #added the ui chat box

---
+++ src/components/ChatBox/ChatBox.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./ChatBox.css";
+import assets from "../../assets/assets";
+
+const ChatBox = () => {
+  return (
+    <div className="chat-box">
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>
+          Anurag Yadav <img className="dot" src={assets.green_dot} alt="" />
+        </p>
+        <img src={assets.help_icon} className="help" alt="" />
+      </div>
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">Hey there....</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">Hey there....</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="chat-input">
+        <input type="text" placeholder="Send a message" />
+        <input type="file" id="image" accept="image/png,image/jpeg" hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default ChatBox;
---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./LeftSidebar.css";
+import assets from "../../assets/assets";
+
+const LeftSidebar = () => {
+  return (
+    <div className="ls">
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} className="logo" alt="Logo" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="Menu" />
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="Search" />
+          <input type="text" placeholder="Search here..." />
+        </div>
+      </div>
+      <div className="ls-list">
+        {Array(15)
+          .fill("")
+          .map((item, index) => (
+            <div key={index} className="friends">
+              <img src={assets.profile_img} alt="Profile" />
+              <div>
+                <p>Anurag Yadav</p>
+                <span>Hello, How are you?</span>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default LeftSidebar;
